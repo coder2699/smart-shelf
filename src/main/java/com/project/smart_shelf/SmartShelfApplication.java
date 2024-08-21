fbd0959f7deb8f8139802f815de42601a728d95e
coder2699 <pranshu0414@gmail.com>
2024-08-22 01:07:50 +0530

    initial commit

---
+++ src/main/java/com/project/smart_shelf/SmartShelfApplication.java
@@ -0,0 +1,13 @@
+package com.project.smart_shelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartShelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/smart_shelf/SmartShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smart_shelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
